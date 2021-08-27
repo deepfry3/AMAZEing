232ac36f4f6ad5eaa7cf33b42b25acf4b7a8b520
Cameron <cameronbennetts@gmail.com>
2021-08-27 15:51:46 +1000

    Add handle rotation
    - Handles now rotate according to maze rotation.
    - Cached some transforms to improve performance.

--- Assets/Scripts/MazeUDController.cs
+++ Assets/Scripts/MazeUDController.cs
@@ -10,9 +10,14 @@ using UnityEngine;
 public class MazeUDController : MonoBehaviour
 {
 	#region Variables
+	// Public variables
+	public GameObject m_Handle = null;						// Physical handle to visually rotate
+
 	// Private variables
-	private Rigidbody m_Rigidbody;						// Rigidbody of the UD Maze
-	private Vector3 m_InputVector = Vector3.zero;       // Up-down input stored as a Vector3
+	private Rigidbody m_Rigidbody;							// Rigidbody of the UD Maze
+	private Transform m_Transform;                          // Cached Transform component
+	private Transform m_HandleTransform;                    // Cached Transform component of the Handle
+	private Vector3 m_InputVector = Vector3.zero;			// Up-down input stored as a Vector3
 	#endregion
 
 	#region Functions
@@ -23,6 +28,10 @@ public class MazeUDController : MonoBehaviour
 		m_Rigidbody = GetComponent<Rigidbody>();
 		m_Rigidbody.centerOfMass = Vector3.zero;
 		m_Rigidbody.inertiaTensorRotation = Quaternion.identity;
+
+		// Cache Transform components to prevent GetComponent being called each Update
+		m_Transform = transform;
+		m_HandleTransform = m_Handle.transform;
 	}
 
 	// Called every frame - Updates input vector according to key-press
@@ -47,7 +56,10 @@ public class MazeUDController : MonoBehaviour
 	void LateUpdate()
 	{
 		// Manually set transform angles, resetting y and z axis to zero
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0.0f, 0.0f);
+		m_Transform.rotation = Quaternion.Euler(m_Transform.eulerAngles.x, 0.0f, 0.0f);
+
+		// Visually update handle rotation
+		m_HandleTransform.rotation = Quaternion.Euler(8.0f * transform.eulerAngles.x, m_Handle.transform.eulerAngles.y, m_Handle.transform.eulerAngles.z);
 	}
 	#endregion
 }
