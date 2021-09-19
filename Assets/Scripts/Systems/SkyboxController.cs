1a715a908867e42bef3a3ba64e32b06a51c0c5d4
Cameron <cameronbennetts@gmail.com>
2021-09-19 17:19:40 +1000

    Overhauled Skybox
    There wasn't much to do, but now it's called SkyboxController

---
+++ Assets/Scripts/Systems/SkyboxController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Author: Blake
+ * 
+ * Manages the rotation of the Skybox camera
+ */
+
+public class SkyboxController : MonoBehaviour
+{
+	#region Variables/Properties
+	// -- Public -- 
+	public float RotateX = 0.01f;								// Rotational speed on X axis
+	public float RotateY = 0.01f;                               // Rotational speed on Y axis
+	public float RotateZ = 0.01f;                               // Rotational speed on Z axis
+	#endregion
+
+	#region Unity Functions
+	/// <summary>
+	/// Called on Update.
+	/// Rotates skybox.
+	/// </summary>
+	void Update()
+    {
+        transform.Rotate(RotateX, RotateY, RotateZ);
+    }
+	#endregion
+}
